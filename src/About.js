83621aa336bcd392669197509636e897037ff91f
beginb101 <dkfltm0907@gmail.com>
2022-09-23 17:52:36 +0900

    update

--- src/About.js
+++ src/About.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import qs from 'qs'; 
+import { useSearchParams } from 'react-router-dom';
 
-const About = ({ location }) => {
-    const query = qs.parse(location.search, {
-        ignoreQueryPrefix: true 
-        // 이 설정을 통해 문자열 맨 앞의 ?를 생략함.
-    });
-    const showDetail = query.detail === 'true'; 
-    // 쿼리의 파싱 결과 값은 문자열입니다
+const About = () => {
+    const searchParams = useSearchParams();
+    const showDetail = searchParams === 'true' ? false : true;
+
     return (
         <div>
         <h1>소개</h1>
--- src/Profile.js
+++ src/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const data = {
   velopert: {
@@ -11,16 +12,17 @@ const data = {
   }
 };
 
-const Profile = ({ match }) => {
-  const { username } = match.params;
-  const profile = data[username];
+const Profile = () => {
+  const params = useParams();
+  const profile = data[params.username];
   if (!profile) {
+    console.log(params);
     return <div>존재하지 않는 사용자입니다.</div>;
   }
   return (
     <div>
       <h3>
-        {username}({profile.name})
+        ({profile.name})
       </h3>
       <p>{profile.description}</p>
     </div>
